2f6b2dc1b611479c1d066f9b5d1af13725025b87
Raphael Reitzig <4246780+reitzig@users.noreply.github.com>
2018-07-12 00:26:34 +0200

    Do not declare function/command sdk if sdkman is not installed.
    Closes issue #6.

--- conf.d/sdk.fish
+++ conf.d/sdk.fish
@@ -1,11 +1,15 @@
 #!/usr/bin/fish
 
-# sdk command
-function sdk
-    bash -c "source $HOME/.sdkman/bin/sdkman-init.sh && sdk $argv"
-end
+set sdkman_init "$HOME/.sdkman/bin/sdkman-init.sh"
 
-# add paths
-for ITEM in $HOME/.sdkman/candidates/* ;
-    set -gx PATH $PATH $ITEM/current/bin
+if test -f "$sdkman_init"
+    # Add candidate binaries to PATH
+    for ITEM in $HOME/.sdkman/candidates/* ;
+        set -gx PATH $PATH $ITEM/current/bin
+    end
+
+    # Declare the sdk command for fish
+    function sdk
+        bash -c "source $sdkman_init && sdk $argv"
+    end
 end
