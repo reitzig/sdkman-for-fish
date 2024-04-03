5c886dcd1c23271a613de766251f8c3754ac3b80
Raphael Reitzig <4246780+reitzig@users.noreply.github.com>
2024-04-03 23:56:16 +0200

    feat(ci): Prevent test runs outside of container
    It's not safe, we write and delete files all over the place!

--- test/features/support/hooks.rb
+++ test/features/support/hooks.rb
@@ -2,6 +2,10 @@ require_relative '../step_definitions/setup'
 require_relative '../step_definitions/corner_cases.rb'
 
 BeforeAll do
+  if ENV.fetch('RUNNING_IN_CONTAINER', false) != 'yessir'
+    Kernel.abort("We don't seem to be running in a container; aborting for safety!")
+  end
+
   run_bash_command('sdk update')
 end
 
@@ -12,7 +16,6 @@ After do |scenario|
   _restore_install
 end
 
-# Uninstall all SDKMAN! candidates
 AfterAll do
   Dir["#{ENV['HOME']}/.sdkman/candidates/*/*"].each do |candidate_dir|
     _uninstall_candidate_version(candidate_dir)
